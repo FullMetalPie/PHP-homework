21ab82abf7ad03b6412d59b29f89ef8f002bb5c3
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-02 18:32:52 +0200

    Completed

--- 08/Es8.php
+++ 08/Es8.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 8 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 8</h1>
+        <p>Scrivere uno script che, dato un numero intero a 3 cifre, restituisca le 3 cifre separate su 3 righe
+        differenti.</p>
         <hr>
         <?php
-            //negro
+            $num = 420;
+            $primaCifra = floor($num / 100);
+            $secondaCifra = floor(($num - ($primaCifra) * 100) / 10);
+            $terzaCifra = floor($num - (($primaCifra * 100) + ($secondaCifra * 10)));
+
+            echo "Numero: $num<br />";
+            echo "<br />Prima cifra: " . $primaCifra;
+            echo "<br />Seconda cifra: " . $secondaCifra;
+            echo "<br />Terza cifra: " . $terzaCifra;
         ?>
     </body>
 </html>

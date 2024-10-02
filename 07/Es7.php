7da415bd6f283a7dd0cf31cbef7e600435408dc0
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-02 18:22:45 +0200

    Completed

--- 07/Es7.php
+++ 07/Es7.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 7 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 7</h1>
+        <p>Effettuare la divisione fra due numeri interi, stampando separatamente quoziente e resto.</p>
         <hr>
         <?php
-            //negro
+            $num1 = 16;
+            $num2 = 9;
+
+            echo "Numero A: $num1;<br />Numero B: $num2.<br />";
+            echo "<br />Quoziente: " . number_format(($num1 / $num2), 2);
+            echo "<br />Resto: " . $num1 % $num2;
         ?>
     </body>
 </html>

c24e6c211085386afd87637683e0a63183dc4ddf
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-02 19:12:58 +0200

    To be continued...

--- 12/Es12.php
+++ 12/Es12.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 12 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 12</h1>
+        <p>Scrivere uno script che calcoli con sufficiente precisione il pi greco (π) sfruttando la formula di
+Nilakantha, riportata di seguito; da notare come la serie di somme e sottrazioni può andare avanti
+all’infinito. Dopo aver individuato come continuare la formula, ricavare π con almeno 4 cifre
+decimali esatte.</p>
         <hr>
         <?php
+            
         ?>
     </body>
 </html>

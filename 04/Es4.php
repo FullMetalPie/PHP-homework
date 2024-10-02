6f0c50773a7b5316bbc8b5ee11cf8d6e4966c189
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-02 18:05:25 +0200

    Completed

--- 04/Es4.php
+++ 04/Es4.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 4 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 4</h1>
+        <p>Si scriva uno script che, dato un numero in virgola mobile, visualizzi gli interi ottenuti
+        approssimando per difetto e per eccesso.</p>
         <hr>
         <?php
-            //negro
+            $num = M_PI;
+
+            echo "Numero: $num <br />";
+            echo "<br />Eccesso: " . ceil($num);
+            echo "<br />Difetto: " . floor($num);
         ?>
     </body>
 </html>

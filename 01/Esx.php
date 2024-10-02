a50639f40397ac30b0982434540eabc45ac758f4
Pie <120573783+FullMetalPie@users.noreply.github.com>
2024-10-02 17:28:14 +0200

    Example file

---
+++ 01/Esx.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <meta charset="UTF-8" />
+    </head>
+    <body>
+        <h1>Esercizio x</h1>
+        <p>traccia</p>
+        <hr>
+        <?php
+            //Esercizio
+        ?>
+    </body>
+</html>

f89d09fc2065dc2100313a265288ab2c19949912
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-02 18:02:01 +0200

    Completed

--- 03/Es3.php
+++ 03/Es3.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 3 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 3</h1>
+        <p>Si scriva uno script che, dato un numero in virgola mobile, visualizzi parte intera e parte frazionaria, separate.</p>
         <hr>
         <?php
-            //negro
+            $phi = 100.144;
+
+            echo "Numero: $phi <br />";
+            echo "<br />Parte intera: " . floor($phi);
+            echo "<br />Parte decimale: " . $phi - floor($phi);
         ?>
     </body>
 </html>

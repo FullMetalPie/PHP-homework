c7116004abdd11bb940cda97f5b3de5bfb1189a5
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-02 18:13:38 +0200

    Completed

--- 05/Es5.php
+++ 05/Es5.php
@@ -6,10 +6,14 @@
     </head>
     <body>
         <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <p>Scrivere uno script che, dato un numero con almeno 4 cifre decimali, lo approssimi alla terza cifra
+        decimale sempre per difetto.<br />Es.: 123.45678 → 123.456</p>
         <hr>
         <?php
-            //negro
+            $num = M_PI;
+
+            echo "Numero: $num <br />";
+            echo "Approssimazione: " . (floor($num * 1000)) / 1000;
         ?>
     </body>
 </html>

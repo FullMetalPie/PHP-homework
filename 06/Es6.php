6bee33107d6fed5cf816b75bad2b7cee3a35f6ee
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-02 18:18:44 +0200

    Completed

--- 06/Es6.php
+++ 06/Es6.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 6 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 6</h1>
+        <p>Scrivere uno script che, dato il prezzo in euro di un articolo in vendita, lo restituisca scontato del
+        10%, 25% e 33%.</p>
         <hr>
         <?php
-            //negro
+            $prezzo = 141.50;
+
+            echo "Prezzo: $prezzo $<br />";
+            echo "<br />Sconto 10%: " . $prezzo - ((10 * $prezzo) / 100) . " $";
+            echo "<br />Sconto 25%: " . $prezzo - ((25 * $prezzo) / 100) . " $";
+            echo "<br />Sconto 33%: " . $prezzo - ((33 * $prezzo) / 100) . " $";
         ?>
     </body>
 </html>

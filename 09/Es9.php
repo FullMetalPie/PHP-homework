684d8aebc6fce95b6ec1fe5f487b717c0fe6e68d
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-02 18:54:28 +0200

    Completed

--- 09/Es9.php
+++ 09/Es9.php
@@ -1,15 +1,31 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 9 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 9</h1>
+        <p>Siano dati due numeri interi di due cifre ab e cd, dove ogni lettera rappresenta nell’ordine una singola cifra (se ab è pari a 74, a vale 7 e b vale 4). 
+            <br />Moltiplicare fra di loro i due numeri implementando l’algoritmo di Karatsuba:<br /> ab · cd = a · c · 10^2 + (a · c + b · d - (a - b) · (c - d)) · 10 + b · d</p>
         <hr>
         <?php
-            //negro
+            $numAB = 11;
+            $numA = floor($numAB / 10);
+            $numB = floor(($numAB - $numA * 10));
+
+            $numCD = 54;
+            $numC = floor($numCD / 10);
+            $numD = floor(($numCD - $numC * 10));
+
+            echo "Numero AB = $numAB";
+            echo "<br />A = $numA";
+            echo "<br />B = $numB";
+            echo "<br /><br />Numero CD = $numCD";
+            echo "<br />C = $numC";
+            echo "<br />D = $numD<br />";
+
+            echo "<br />Moltiplicazione tramite l'algoritmo di Karatsuba: " . ($numA * $numC * pow(10, 2) + ($numA * $numC + $numB * $numD - ($numA - $numB) * ($numC * $numD)) * 10 + $numB * $numD);
         ?>
     </body>
 </html>

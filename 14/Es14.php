263085c2b05475a50d19dba8219b300edcd340c3
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-04 11:47:16 +0200

    Completed

--- 14/Es14.php
+++ 14/Es14.php
@@ -1,14 +1,27 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 14 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 14</h1>
+        <p>Si scriva uno script che, dati quattro numeri, visualizzi la loro media geometrica, ovvero la radice
+        n-esima del prodotto di n valori.</p>
         <hr>
         <?php
+            $num1 = random_int(1, 6);
+            $num2 = random_int(1, 6);
+            $num3 = random_int(1, 6);
+            $num4 = random_int(1, 6);
+            $prodotto = $num1 * $num2 * $num3 * $num4;
+
+            echo "Numero 1: $num1;<br />";
+            echo "Numero 2: $num2;<br />";
+            echo "Numero 3: $num3;<br />";
+            echo "Numero 4: $num4;<br />";
+
+            echo "<br />Media geometrica: " . number_format(sqrt(sqrt($prodotto)), 2);
         ?>
     </body>
 </html>

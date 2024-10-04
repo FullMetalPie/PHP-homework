07d4f12ef96f5c83422e3e0a545d0e6dc880c1bf
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-04 11:23:26 +0200

    To do

--- 12/Es12.php
+++ 12/Es12.php
@@ -10,6 +10,7 @@
 Nilakantha, riportata di seguito; da notare come la serie di somme e sottrazioni può andare avanti
 all’infinito. Dopo aver individuato come continuare la formula, ricavare π con almeno 4 cifre
 decimali esatte.</p>
+        <img src="img.png" width="400px">
         <hr>
         <?php
             

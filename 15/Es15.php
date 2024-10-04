5708f371b58872aca2e178f27c68bf43a0296ca4
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-04 11:52:05 +0200

    To be continued

--- 15/Es15.php
+++ 15/Es15.php
@@ -1,14 +1,30 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 15 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 15</h1>
+        <p>Si scriva uno script che, dati cinque valori interi, visualizzi la loro media armonica, ovvero il
+        reciproco della media aritmetica dei reciproci.</p>
         <hr>
         <?php
+            $num1 = random_int(1, 10);
+            $num2 = random_int(1, 10);
+            $num3 = random_int(1, 10);
+            $num4 = random_int(1, 10);
+            $num5 = random_int(1, 10);
+
+            echo "Numero 1: $num1;<br />";
+            echo "Numero 2: $num2;<br />";
+            echo "Numero 3: $num3;<br />";
+            echo "Numero 4: $num4;<br />";
+            echo "Numero 5: $num5;<br />";
+
+            
+
+            echo "<br />Media armonica: "
         ?>
     </body>
 </html>

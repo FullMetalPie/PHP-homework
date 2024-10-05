77a330225aad859e12ce6cc7037c1d3786e3fe97
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-05 08:35:12 +0200

    Completed

--- 18/Es18.php
+++ 18/Es18.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 18 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 18</h1>
+        <p>Generare casualmente un voto compreso fra 1 e 10 (includere i “mezzi voti”).</p>
         <hr>
         <?php
+            $voto = (random_int(1, 10) * (1 / 2));
+
+            echo "Valutazione: $voto";
         ?>
     </body>
 </html>

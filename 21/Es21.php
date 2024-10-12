531f70427275b22aee1b8985de3ffcaabf310dde
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-12 09:40:20 +0200

    Completed

--- 21/Es21.php
+++ 21/Es21.php
@@ -1,15 +1,42 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 21 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 21</h1>
+        <p>Dati tre interi stabilire se possono essere le lunghezze dei lati di un triangolo. In un triangolo, un
+        lato è sempre minore della somma degli altri due.</p>
         <hr />
         <?php
+        $lato1 = random_int(1, 10);
+        $lato2 = random_int(1, 10);
+        $lato3 = random_int(1, 10);
+
+        echo "<p>Lato 1: $lato1;<br />Lato 2: $lato2;<br />Lato 3: $lato3.</p>";
         
+        echo "<p>";
+
+        if ($lato1 < ($lato2 + $lato3)) {
+            echo "Lato 1 valido.<br />";
+        } else {
+            echo "Lato 1 non valido.<br />";
+        }
+        
+        if ($lato2 < ($lato1 + $lato3)) {
+            echo "Lato 2 valido.<br />";
+        } else {
+            echo "Lato 2 non valido.<br />";
+        }
+
+        if ($lato3 < ($lato2 + $lato2)) {
+            echo "Lato 3 valido.<br />";
+        } else {
+            echo "Lato 3 non valido.<br />";
+        }
+
+        echo "</p>";
         ?>
     </body>
 </html>
--- 22/Es22.php
+++ 22/Es22.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 22 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 22</h1>
+        <p>Scrivere un programma in grado di calcolare a<sup>b</sup>, con a e b interi, senza l’utilizzo della funzione
+        pow() e dell’operatore **.</p>
         <hr />
         <?php
+        $numA = random_int(2, 9);
+        $numB = random_int(2, 9);
+
+        echo "<p>Numero A (base): $numA;<br />Numero B (esponente): $numB.</p>";
         
+        $result = $numA;
+        for ($i = 1; $i < $numB; $i++) {
+            $result *= $numA;
+        }
+        
+        echo "<p>$numA<sup>$numB</sup> = $result</p>";
         ?>
     </body>
 </html>

b28d6018383cacefc906530789dbbad64cfc4554
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-18 11:27:13 +0200

    Completed

--- 28/Es28.php
+++ 28/Es28.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 28 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 28</h1>
+        <p>Scrivere uno script in grado di ricavare il numero di cifre di un dato intero positivo</p>
         <hr />
         <?php
-        
+        $num = random_int(1, 10000);
+        echo "<p>Numero randomico: $num<br />";
+        echo "Numero di cifre: " . floor(log10($num)) + 1 . "</p>";
         ?>
     </body>
 </html>

f5b6757e37f65e15ec218fdaf1b5010e992aabbd
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-18 11:13:27 +0200

    Completed

--- 27/Es27.php
+++ 27/Es27.php
@@ -1,15 +1,64 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 27 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 27</h1>
+        <p>Fornita una data in formato numerico, comprendendo anche il giorno della settimana,
+        determinare quella successiva, senza considerare l’anno. Es.:</p>
         <hr />
         <?php
-        
+        $mese = random_int(1, 12);
+        $mese31G = false;
+        $mese28G = false;
+        switch ($mese) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                $mese31G = true;
+                $giorno = random_int(1,31);
+                break;
+            case 2:
+                $mese28G = true;
+                $giorno = random_int(1, 28);
+                break;
+            default:
+                $giorno = random_int(1, 30);
+        }
+
+        echo "<p>Data: $giorno/$mese<br />";
+
+        if ($mese31G = true && $giorno == 31) {
+            if ($mese == 12) {
+                $mese = 1;
+                $giorno = 1;
+            } else {
+                $mese++;
+                $giorno = 1;
+            }
+        } else if ($giorno != 31) {
+            $giorno++;
+        } else if ($mese31G == true && $giorno != 31) {
+            $giorno++;
+        } else if ($mese28G == true && $giorno == 28) {
+            $mese++;
+            $giorno = 1;
+        } else {
+            if ($giorno == 30) {
+                $mese++;
+                $giorno = 1;
+            } else {
+                $giorno++;
+            }
+        }
+
+        echo "Data successiva: $giorno/$mese</p>";
         ?>
     </body>
 </html>

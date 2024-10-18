db089d83798ba92252d63a881ce59380973e2555
FullMetalPie <andrea.pietri06@gmail.com>
2024-10-18 10:31:57 +0200

    Completed

--- 24/Es24.php
+++ 24/Es24.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Esercizio x | Pietri Andrea 5AI</title>
+        <title>Esercizio 24 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
     </head>
     <body>
-        <h1>Esercizio x</h1>
-        <p>traccia</p>
+        <h1>Esercizio 24</h1>
+        <p>Generare casualmente una sequenza di 1000 nucleotidi (rappresentati dalle lettere A, C, G e T) di
+        un filamento di DNA.</p>
         <hr />
         <?php
+        echo "<p>";
+        for ($i = 0; $i < 1000; $i++) {
+            $lettera = random_int(1, 4);
+            $dna = match ($lettera) {
+                1 => "A",
+                2 => "C",
+                3 => "G",
+                4 => "T",
+            };
+            echo "$dna";
+        }
         
+        echo "</p>";
         ?>
     </body>
 </html>

9e920ac56c01cd1aff5f86f797169981177842c9
FullMetalPie <andrea.pietri06@gmail.com>
2024-11-15 11:52:29 +0100

    To be continued

---
+++ 44/Es44.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Esercizio 44 | Pietri Andrea 5AI</title>
+        <meta charset="UTF-8" />
+    </head>
+    <body>
+        <h1>Esercizio x</h1>
+        <p>Generare 10 numeri interi pseudo-casuali <span style="color:red">senza ripetizione</span> compresi fra 1 e 10</p>
+        <hr />
+        <?php
+        $arr = [];
+
+        for ($i = 0; ) {
+
+        }
+        ?>
+    </body>
+</html>

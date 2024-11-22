0886f26384d22b69fb17b37e704990c315fe33f7
FullMetalPie <andrea.pietri06@gmail.com>
2024-11-22 19:54:30 +0100

    To do

---
+++ 55/Es55.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Esercizio 55 | Pietri Andrea 5AI</title>
+        <meta charset="UTF-8" />
+    </head>
+    <body>
+        <h1>Esercizio 55</h1>
+        <p>Scrivere un programma PHP che costruisca e stampi, data una matrice quadrata di N × N
+            elementi, la trasposta di tale matrice. La trasposta di una matrice è una matrice in cui le
+            posizioni degli elementi sono modificate scambiando gli indici di riga e di colonna: <br />
+            
+        </p>
+        <hr />
+        <?php
+        
+        ?>
+    </body>
+</html>

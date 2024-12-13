2e5b4d46118c322f4a4a42d49595e93489a62d51
FullMetalPie <andrea.pietri06@gmail.com>
2024-12-13 16:17:58 +0100

    Completed

--- 62/form.php
+++ 62/form.php
@@ -3,14 +3,29 @@
     <head>
         <title>Esercizio 62 | Pietri Andrea 5AI</title>
         <meta charset="UTF-8" />
+        <style>
+            form {
+                border: 1px solid black;
+                width: fit-content;
+                padding: 1em;
+            }
+            label {
+                font-weight: bold;
+            }
+        </style>
     </head>
     <body>
         <h1>Esercizio 62</h1>
         <h2 style="font-style: oblique; color: blue">Invio dati</h2>
-        <p></p>
+        <p><strong>YouTube URL shortener</strong>.<br /> Realizzare un’applicazione che permetta di abbreviare gli URL dei video
+            di YouTube. L’utente dovrà fornire l’indirizzo del video, ottenendo un link funzionante.
+        </p>
         <hr />
         <form name="dati" action="output.php" method="post">
-            
+            <label>Inserisci un link youtube:<br />
+                <input type="textarea" name="link" required="required" placeholder="https://www.youtube.com/...">
+            </label><br />
+            <input type="submit" value="Invia">
         </form>
     </body>
 </html>
--- 62/output.php
+++ 62/output.php
@@ -7,69 +7,32 @@
     <body>
         <h1>Esercizio 62</h1>
         <h2 style="font-style: oblique; color: blue">Ricezione dati</h2>
-        <p>Scrivere un’applicazione PHP per verificare se un indirizzo IPv4 sia valido o meno; individuarne,
-        inoltre, la classe di appartenenza.</p>
+        <p><strong>YouTube URL shortener</strong>. Realizzare un’applicazione che permetta di abbreviare gli URL dei video
+            di YouTube. L’utente dovrà fornire l’indirizzo del video, ottenendo un link funzionante.
+        </p>
         <hr />
         <?php
-        $valido = true;
-        
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            if ($_POST["primo"] === "") {
-                $_DATA["primo"] = "Null";
-                $valido = false;
+            if (empty($_POST["link"])) {
+                $_DATA["link"] = "Nessun link inserito.";
             } else {
-                $_DATA["primo"] = $_POST["primo"];
-            }
-
-            if ($_POST["secondo"] === "") {
-                $_DATA["secondo"] = "Null";
-                $valido = false;
-            } else {
-                $_DATA["secondo"] = $_POST["secondo"];
-            }
-
-            if ($_POST["terzo"] === "") {
-                $_DATA["terzo"] = "Null";
-                $valido = false;
-            } else {
-                $_DATA["terzo"] = $_POST["terzo"];
-            }
-
-            if ($_POST["quarto"] === "") {
-                $_DATA["quarto"] = "Null";
-                $valido = false;
-            } else {
-                $_DATA["quarto"] = $_POST["quarto"];
+                $_DATA["link"] = $_POST["link"];
             }
         }
-        // Scrittura dati
 
-        $ip = $_DATA["primo"] . "." . $_DATA["secondo"] . "." . $_DATA["terzo"] . "." . $_DATA["quarto"];
-        echo "<p><strong>Ip inserito: </strong>";
+        echo "<p><strong>Link inserito:</strong><a href=\"" . $_DATA["link"] . "\" target=\"blank_\">" . $_DATA["link"] . "</a><br/>";
+        $str = $_DATA["link"];
+        $str = explode("/", $str);
 
-        if (!$valido) {
-            echo "Indirizzo IP non valido!";
-        } else {
-            echo "Indirizzo IP valido";
-        }
+        $str[2] = "youtu.be";
+        $_DATA["link_short"] = $str[0] .= "//" . $str[2];
 
-        echo "<br />[<strong>" . $_DATA["primo"] . "</strong>]";
-        echo "[<strong>" . $_DATA["secondo"] . "</strong>]";
-        echo "[<strong>" . $_DATA["terzo"] . "</strong>]";
-        echo "[<strong>" . $_DATA["quarto"] . "</strong>]";
-
-        // Classe indirizzo
-        echo "<br />L'indirizzo inserito è di ";
-
-        $class = match(true) {
-            $_DATA["primo"] >= 1 && $_DATA["primo"] < 127 => "Classe A.",
-            $_DATA["primo"] >= 128 && $_DATA["primo"] < 191 => "Classe B.",
-            $_DATA["primo"] >= 192 && $_DATA["primo"] < 223 => "Classe C.",
-            $_DATA["primo"] >= 224 && $_DATA["primo"] < 239 => "Classe D.",
-            $_DATA["primo"] >= 240 => "Classe E."
-        };
-
-        echo $class . "</p>";
+        $str_linkCode = explode("=", $str[3]);
+        unset($str_linkCode[0]);
+        
+        $_DATA["link_short"] .=  "/" . $str_linkCode[1];
+        
+        echo "<strong>Link abbreviato:</strong><a href=\"" . $_DATA["link_short"] . "\" target=\"blank_\">" . $_DATA["link_short"] . "</a></p>";
         ?>
     </body>
 </html>
